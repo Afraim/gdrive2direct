af63cf0d15713409bcef6041f87b07f06d58f219
Afraim Zahangir <afraim.zahangir@gmail.com>
2022-08-07 15:26:00 +0600

    URL verify and copy only when url can be converted

--- JS/myjs.js
+++ JS/myjs.js
@@ -1,10 +1,18 @@
         function validation(){
             let text = document.querySelector("input").value;
             if(text != ""){
-                value(text);
+                let url;
+  
+                try {
+                  url = new URL(text);
+                  value(url.toString());
+                } catch (_) {
+                   return false;  
+                }
+                
             }
             else{
-                document.getElementById("Dlink").innerHTML = "Empty!";
+                document.getElementById("Dlink").value = "Empty!";
             }
         }    
 
@@ -12,11 +20,20 @@
             var Array = T.split("/");
             console.log(Array);
             const TEXT = "https://drive.google.com/uc?export=download&id=";
-            document.getElementById("Dlink").value = TEXT + Array[5];
+            if(TEXT + Array[5]!="https://drive.google.com/uc?export=download&id=undefined"){
+                document.getElementById("Dlink").value = TEXT + Array[5];
+                document.getElementById("afterSubmit").style.display = "block";
+            }
+            else{
+                document.getElementById("Dlink").value = "";
+                document.getElementById("afterSubmit").style.display = "none";    
+            }
         }
         function myFunction() {
-        /* Get the text field */
-             var copyText = document.getElementById("Dlink");
+        let text = document.getElementById("Dlink").value;
+        if(text != "" && text != "Empty!"){
+            /* Get the text field */
+            var copyText = document.getElementById("Dlink");
 
             /* Select the text field */
             copyText.select();
@@ -24,10 +41,11 @@
 
             /* Copy the text inside the text field */
             navigator.clipboard.writeText(copyText.value);
-            document.getElementById("copy").innerHTML = "Copied";
+            document.getElementById("copy").innerHTML = "✔Copied";
             setTimeout(() => {
                 document.getElementById("copy").innerHTML = "Copy";
                 
             }, 1000);
+        }
             
 }
